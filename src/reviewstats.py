be3a384d4bb12c7478ed2d6db0bdc5c123076391
William Martin <will.st4@gmail.com>
2017-11-25 20:36:01 +0000

    Created dataframe from PR merge commits

--- src/reviewstats.py
+++ src/reviewstats.py
@@ -1,9 +1,16 @@
+import logging
 import os
+import re
+from collections import namedtuple
 from functools import partial
 from operator import is_not
 
 import click
-import re
+import pandas as pd
+
+COMMIT_COLUMNS = ['author', 'date', 'reviewers', 'title']
+
+logging.basicConfig(level=logging.INFO)
 
 LOG_FILENAME = 'aed.log'
 merge_regex = re.compile('commit (\S){40}\\n')
@@ -12,33 +19,24 @@ date_regex = re.compile('Date:\s+(.*)\\n')
 reviewer_regex = re.compile('Approved-by:\s+(\w+ \w+ <[\w\d]+.?[\w\d]+@[\w\d]+.[\w\d]+>)')
 title_regex = re.compile('Merged in [\w/-]+ \(pull request #[\d]+\)\\n\s+\\n\s+([\w\s\d]*)\\n')
 
+commit_structure = namedtuple('Commit', COMMIT_COLUMNS)
 
-class Commit(object):
-    author = ''
-    date = ''
-    reviewers = ''
-    title = ''
 
-    def __init__(self, author, date, reviewers, title):
-        self.author = author
-        self.date = date
-        self.reviewers = reviewers
-        self.title = title
-
+class Commit(commit_structure):
     def __repr__(self):
         return 'Commit by {author} on {date}, reviewed by {reviewers}: {title}'.format(
             author=self.author, date=self.date, reviewers=self.reviewers, title=self.title)
 
 
 def parse_commit(text):
-        try:
-            author = author_regex.search(text).group(1)
-            date = date_regex.search(text).group(1)
-            reviewers = reviewer_regex.findall(text)
-            title = title_regex.search(text).group(1)
-            return Commit(author, date, reviewers, title)
-        except:
-            return None
+    try:
+        author = author_regex.search(text).group(1)
+        date = date_regex.search(text).group(1)
+        reviewers = reviewer_regex.findall(text)
+        title = title_regex.search(text).group(1)
+        return Commit(author, date, reviewers, title) if len(reviewers) > 0 else None
+    except:
+        return None
 
 
 def get_commit_log(directory):
@@ -58,12 +56,22 @@ def get_merges(commit_log):
     return result
 
 
+def objects_to_dateframe(merges):
+    df = pd.DataFrame(merges, columns=COMMIT_COLUMNS)
+    # df.set_index(commit_structure).unstack(level=1)
+    return df
+
+
 @click.command()
 @click.option('--directory')
 def main(directory):
     log_text = get_commit_log(directory)
     merges = get_merges(log_text)
-    i = 10
+    logging.info("Extracted {no_merges:d} merged pull requests".format(no_merges=len(merges)))
+
+    merge_df = objects_to_dateframe(merges)
+    merge_df[['date', 'author']].set_index('date').plot()
+
 
 if __name__ == '__main__':
     main()
