46d3eec9329c137c762d945ea0f81491aa7b03fd
William Martin <will.st4@gmail.com>
2017-11-26 11:29:37 +0000

    Graphed various stats

--- src/reviewstats.py
+++ src/reviewstats.py
@@ -8,7 +8,7 @@ from operator import is_not
 import click
 import pandas as pd
 
-COMMIT_COLUMNS = ['author', 'date', 'reviewers', 'title']
+COMMIT_COLUMNS = ['author', 'date', 'reviewers', 'title', 'no_reviews']
 
 logging.basicConfig(level=logging.INFO)
 
@@ -24,8 +24,8 @@ commit_structure = namedtuple('Commit', COMMIT_COLUMNS)
 
 class Commit(commit_structure):
     def __repr__(self):
-        return 'Commit by {author} on {date}, reviewed by {reviewers}: {title}'.format(
-            author=self.author, date=self.date, reviewers=self.reviewers, title=self.title)
+        return 'Commit by {author} on {date}, reviewed by {no_reviews}: {title}'.format(
+            author=self.author, date=self.date, reviewers=self.no_reviews, title=self.title)
 
 
 def parse_commit(text):
@@ -34,7 +34,8 @@ def parse_commit(text):
         date = date_regex.search(text).group(1)
         reviewers = reviewer_regex.findall(text)
         title = title_regex.search(text).group(1)
-        return Commit(author, date, reviewers, title) if len(reviewers) > 0 else None
+        no_reviews = len(reviewers)
+        return Commit(author, date, reviewers, title, no_reviews) if no_reviews > 0 else None
     except:
         return None
 
@@ -52,25 +53,70 @@ def get_commit_log(directory):
 
 def get_merges(commit_log):
     result = [parse_commit(c) for c in merge_regex.split(commit_log)]
-    result = filter(partial(is_not, None), result)
+    result = list(filter(partial(is_not, None), result))
     return result
 
 
 def objects_to_dateframe(merges):
     df = pd.DataFrame(merges, columns=COMMIT_COLUMNS)
+    df['date'] = pd.to_datetime(df['date'], errors='coerce')
     # df.set_index(commit_structure).unstack(level=1)
     return df
 
 
 @click.command()
 @click.option('--directory')
-def main(directory):
+@click.option('--output')
+def main(directory, output):
     log_text = get_commit_log(directory)
     merges = get_merges(log_text)
     logging.info("Extracted {no_merges:d} merged pull requests".format(no_merges=len(merges)))
 
     merge_df = objects_to_dateframe(merges)
-    merge_df[['date', 'author']].set_index('date').plot()
+
+    output = os.path.abspath(output)
+    try:
+        os.makedirs(output)
+    except OSError:
+        pass
+
+    # overall PR histogram
+    merge_df.set_index(['date'], inplace=True)
+    ax = merge_df.groupby(pd.TimeGrouper(freq='M'))['author'].count().plot()
+    ax.set_ylabel('no merged pull requests')
+    ax.set_title('No merged pull requests by month')
+    ax.get_figure().savefig(os.path.join(output, 'prs.png'))
+
+    # PRs by author
+    ax = merge_df.groupby([pd.TimeGrouper(freq='M'), 'author']).count()['title'].unstack('author').plot()
+    ax.set_ylabel('no merged pull requests')
+    ax.set_title('No merged pull requests by author by month')
+    ax.get_figure().savefig(os.path.join(output, 'prs_by_author.png'))
+
+    # avg reviews by month
+    ax = merge_df.groupby(pd.TimeGrouper(freq='M'))['no_reviews'].mean().plot()
+    ax.set_ylabel('no_reviews')
+    ax.set_title('Avg reviews by month')
+    ax.get_figure().savefig(os.path.join(output, 'avg_reviews.png'))
+
+    # authors by month
+    ax = merge_df.groupby([pd.TimeGrouper(freq='M'), 'author'])['author'].size().groupby(level=0).size().plot()
+    ax.set_ylabel('no authors')
+    ax.set_title('No authors by month')
+    ax.get_figure().savefig(os.path.join(output, 'authors.png'))
+
+    # reviews / author by month
+    ax = (merge_df.groupby(pd.TimeGrouper(freq='M'))['no_reviews'].mean() /
+     merge_df.groupby([pd.TimeGrouper(freq='M'), 'no_reviews'])['no_reviews'].size().groupby(level=0).size()).plot()
+    ax.set_ylabel('avg reviews / no authors')
+    ax.set_title('Average reviews / no authors by month')
+    ax.get_figure().savefig(os.path.join(output, 'avg_reviews_by_authors.png'))
+
+    (merge_df.groupby(pd.TimeGrouper(freq='M'))['no_reviews'].sum() /
+     merge_df.groupby([pd.TimeGrouper(freq='M'), 'no_reviews'])['no_reviews'].size().groupby(level=0).size()).plot()
+    ax.set_ylabel('total reviews / authors')
+    ax.set_title('Total reviews / authors by month')
+    ax.get_figure().savefig(os.path.join(output, 'total_reviews_by_authors.png'))
 
 
 if __name__ == '__main__':
