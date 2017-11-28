5bf1e73cd84bbcfc39d1a7125ff915631ff1950f
William Martin <will.st4@gmail.com>
2017-11-28 20:40:49 +0000

    Added metrics to the web page

--- src/graphs.py
+++ src/graphs.py
@@ -1,4 +1,5 @@
 """ Graphing functions """
+import calendar
 import os
 
 import matplotlib.pyplot as plt
@@ -17,6 +18,20 @@ def plot_review_stats(df, output):
     time_author_grouped_df = df.groupby([pd.TimeGrouper(freq='M'), gitparser.AUTHOR])
     reviewer_cols = list(df[df.columns.difference(gitparser.COMMIT_COLUMNS)].columns)
 
+    prs_by_author = time_author_grouped_df.count()[gitparser.TITLE].unstack(gitparser.AUTHOR)
+    reviews_by_person = time_grouped_df[reviewer_cols].sum()
+    reviews_by_prs = (reviews_by_person / prs_by_author).dropna(axis=1, how="all")
+    # metrics
+    contributer_of_the_month = prs_by_author.idxmax(axis=1)
+    reviewer_of_the_month = reviews_by_person.idxmax(axis=1)
+    selfless_reviewer = reviews_by_prs.idxmax(axis=1)
+    metrics_df = pd.DataFrame(data={'top contributor': contributer_of_the_month, 'top reviewer': reviewer_of_the_month,
+                                    'selfless reviewer': selfless_reviewer})
+    metrics_df.index = ['{year} {month}'.format(year=idx.year, month=calendar.month_name[idx.month])
+                        for idx in metrics_df.index]
+    with open(os.path.join(output, 'metrics.html'), 'w') as f:
+        f.write(metrics_df.to_html())
+
     # overall PR histogram
     ax = time_grouped_df[gitparser.AUTHOR].count().plot()
     ax.set_ylabel('no. merged pull requests')
@@ -26,7 +41,6 @@ def plot_review_stats(df, output):
     plt.close()
 
     # PRs by author
-    prs_by_author = time_author_grouped_df.count()[gitparser.TITLE].unstack(gitparser.AUTHOR)
     ax = prs_by_author.fillna(0.).plot()
     ax.set_ylabel('no. merged pull requests')
     ax.set_title('No. merged pull requests by author per month')
@@ -37,7 +51,6 @@ def plot_review_stats(df, output):
     plt.close()
 
     # reviews by reviewer
-    reviews_by_person = time_grouped_df[reviewer_cols].sum()
     ax = reviews_by_person.fillna(0.).plot()
     ax.set_ylabel('no. reviews')
     ax.set_title('No. reviews by reviewer per month')
@@ -48,7 +61,7 @@ def plot_review_stats(df, output):
     plt.close()
 
     # reviews / PRs by author
-    ax = (reviews_by_person / prs_by_author).dropna(axis=1, how="all").plot()
+    ax = reviews_by_prs.plot()
     ax.set_ylabel('reviews / prs')
     ax.set_title('Reviews / PRs by author per month')
     plt.gca().set_ylim(bottom=-1)
