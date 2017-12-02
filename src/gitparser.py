6d09b033b4c7f02dfcbe1c8acee58e74f88f8a9b
William Martin <will.st4@gmail.com>
2017-12-02 10:51:28 +0000

    Improved parser

--- src/gitparser.py
+++ src/gitparser.py
@@ -1,36 +1,46 @@
 """ Functions for parsing git commit messages """
+import logging
 import re
 from collections import namedtuple
 from functools import partial
 from operator import is_not
 
+logger1 = logging.getLogger('git log parser')
+
 # regular expressions for parsing git commit messages (bitbucket merges tested)
-merge_regex = re.compile('commit (\S){40}\\n')
-author_regex = re.compile('Author:\s+([\w\s]*\w)\s?[\<\n]')
-date_regex = re.compile('Date:\s+(.*)\\n')
-reviewer_regex = re.compile('Approved-by:\s+([\w ]+)')
+commit_regex = re.compile('commit\s(\S+)\s')
+merge_regex = re.compile('[Mm]erge')
+pr_regex = re.compile('pull request')
+author_regex = re.compile('Author:\s+([\w\s]*\w)\s?[\<\\n]')
+date_regex = re.compile('Date:\s+(.*)\n')
 title_regex = re.compile('Merged in [\S]+ \(pull request #[\d]+\)\s+((\<[\w+\s]+\>)?[\w\s\d.]*)\s')
+reviewer_regex = re.compile('Approved-by:\s+([\w ]+)')
 
 # indexing constants
+HASH = 'commit_hash'
 AUTHOR = 'author'
 DATE = 'date'
 NO_REVIEWS = 'no_reviews'
 REVIEWERS = 'reviewers'
 TITLE = 'title'
+FILES = 'files_changed'
+INSERTIONS = 'insertions'
+DELETIONS = 'deletions'
 
 # storage for commits
-COMMIT_COLUMNS = [AUTHOR, DATE, REVIEWERS, TITLE, NO_REVIEWS]
+COMMIT_COLUMNS = [HASH, AUTHOR, DATE, TITLE, REVIEWERS, NO_REVIEWS, FILES, INSERTIONS, DELETIONS]
 commit_structure = namedtuple('Commit', COMMIT_COLUMNS)
 
 
 class Commit(commit_structure):
     """ Represents a git pull request merge commit """
+
     def __repr__(self):
         return 'Commit by {author} on {date}, reviewed by {no_reviews}: {title}'.format(
             author=self.author, date=self.date, reviewers=self.no_reviews, title=self.title)
 
 
-def parse_commit(text):
+def parse_commit(commit_hash, text):
     """ Parses the given commit text
     :param str text: the text to parse
     :return: a Commit with extracted relevant fields, or None if an error occurred
@@ -39,12 +49,19 @@ def parse_commit(text):
     try:
         author = author_regex.search(text).group(1)
         date = date_regex.search(text).group(1)
+        title = title_regex.search(text).group(1).strip()
         reviewers = [r.strip().replace('\n', '') for r in reviewer_regex.findall(text)]
-        title = title_regex.search(text).group(1)
+        # no self reviews allowed in these stats
+        try:
+            reviewers.remove(author)
+        except ValueError:
+            pass
         no_reviews = len(reviewers)
-        return Commit(author, date, reviewers, title, no_reviews) if no_reviews > 0 else None
+        if no_reviews > -1:
+            return Commit(commit_hash, author, date, title, reviewers, no_reviews, 0, 0, 0)
     except:
-        return None
+        pass
+    return None
 
 
 def extract_pr_commits(commit_log):
@@ -53,6 +70,13 @@ def extract_pr_commits(commit_log):
     :return: a list of pull request merge commits
     :rtype: list[Commit]
     """
-    results = [parse_commit(c) for c in merge_regex.split(commit_log)]
+    commits = commit_regex.split(commit_log)[1:]
+    # form tuples of commit hash, log
+    commits = list(zip(commits[::2], commits[1::2]))
+    logger1.info('Extracted {no_merges} merges'.format(no_merges=len(commits)))
+    results = [parse_commit(*c) for c in commits]
     results = list(filter(partial(is_not, None), results))
+
+    logger1.info('Extracted {no_merges} PRs'.format(no_merges=len(results)))
+
     return results
