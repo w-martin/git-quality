664d03397ed7ace9abb7a9ec0eb5dba3c0234ffc
William Martin <will.st4@gmail.com>
2018-01-28 12:30:47 +0000

    Updated graphs to be more useful

--- src/graphs.py
+++ src/graphs.py
@@ -1,8 +1,7 @@
 """ Graphing functions """
-import calendar
+import datetime
 import os
 
-import datetime
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -21,8 +20,8 @@ def plot_review_stats(df, output):
     recent_authors = np.sort(df[df.index > date_threshold][gitparser.AUTHOR].unique())
 
     # groupings
-    time_grouped_df = df.groupby(pd.TimeGrouper(freq='M'))
-    time_author_grouped_df = df.groupby([pd.TimeGrouper(freq='M'), gitparser.AUTHOR])
+    time_grouped_df = df.groupby(pd.Grouper(freq='M'))
+    time_author_grouped_df = df.groupby([pd.Grouper(freq='M'), gitparser.AUTHOR])
     reviewer_cols = list(df[df.columns.difference(gitparser.COMMIT_COLUMNS)].columns)
 
     prs_by_author = time_author_grouped_df[gitparser.TITLE].count().unstack(gitparser.AUTHOR).loc[:, recent_authors]
@@ -32,68 +31,77 @@ def plot_review_stats(df, output):
 
     # overall PR histogram
     pr_df = time_grouped_df[gitparser.AUTHOR].count()[:-1]
-    ax = pr_df.plot(linewidth=2)
+    xticklabels = [dt.strftime("%b'%y") if (i % 3 == 0) else '' for i, dt in enumerate(pr_df.index)]
+    fig, ax = plt.subplots(figsize=(7, 4))
+    pr_df.plot(colormap='tab10', linewidth=3, ax=ax, kind='bar')
+    ax.set_xticklabels(xticklabels, rotation=0)
     ax.set_ylabel('no. pull requests')
     ax.set_title('No. merged pull requests per month')
     plt.gca().set_ylim(bottom=0)
-    ax.get_figure().savefig(os.path.join(output, 'prs.png'), bbox_inches='tight')
+    fig.savefig(os.path.join(output, 'prs.png'), bbox_inches='tight')
     plt.close()
 
     # avg reviews by month
-    avg_reviews_df = time_grouped_df[gitparser.NO_REVIEWS].mean()[:-1]
-    ax = avg_reviews_df.plot(colormap='tab10', linewidth=2)
+    reviews_mean = time_grouped_df[gitparser.NO_REVIEWS].mean()[:-1].as_matrix()
+    reviews_std = time_grouped_df[gitparser.NO_REVIEWS].std()[:-1].as_matrix()
+    fig, ax = plt.subplots(figsize=(7, 4))
+    ax.errorbar(x=pr_df.index, y=reviews_mean, yerr=reviews_std, fmt='o',
+                markersize=8, capsize=8)
+    ax.set_xticks(pr_df.index)
+    ax.set_xticklabels([], minor=1)
+    ax.set_xticklabels(xticklabels, rotation=0)
     ax.set_ylabel(gitparser.NO_REVIEWS)
     ax.set_title('Avg reviews per month')
     plt.gca().set_ylim(bottom=0)
-    ax.get_figure().savefig(os.path.join(output, 'avg_reviews.png'), bbox_inches='tight')
+    fig.savefig(os.path.join(output, 'avg_reviews.png'), bbox_inches='tight')
     plt.close()
-
-    # grab x labels for bar chart
-    x_labels_0 = ax.get_xticklabels(0)[::-1]
-    x_labels_1 = ax.get_xticklabels(1)
-    for i in range(len(x_labels_1)):
-        if len(x_labels_1[i].get_text()) == 0:
-            x_labels_1[i] = x_labels_0.pop()
 
     # authors by month
     authors_df = time_author_grouped_df[gitparser.AUTHOR].size().groupby(level=0).size()[:-1]
-    ax = authors_df.plot(colormap='tab10', linewidth=2, kind='bar')
-    # fix labels
-    ax.set_xticklabels(x_labels_1, minor=0)
-
+    fig, ax = plt.subplots(figsize=(7, 4))
+    authors_df.plot(colormap='tab10', linewidth=2, kind='bar', ax=ax)
+    ax.set_xticklabels(xticklabels, rotation=0)
     ax.set_ylabel('no. authors')
     ax.set_title('No. authors per month')
     plt.gca().set_ylim(bottom=0)
-    ax.get_figure().savefig(os.path.join(output, 'authors.png'), bbox_inches='tight')
+    fig.savefig(os.path.join(output, 'authors.png'), bbox_inches='tight')
     plt.close()
 
     # reviews / author by month
     reviews_author_df = (time_grouped_df[gitparser.NO_REVIEWS].sum() / authors_df)[:-1]
-    ax = reviews_author_df.plot(colormap='tab10', linewidth=2)
+    fig, ax = plt.subplots(figsize=(7, 4))
+    reviews_author_df.plot(colormap='tab10', linewidth=2, ax=ax)
+    ax.set_xticks(pr_df.index)
+    ax.set_xticklabels(xticklabels, rotation=0)
+    ax.set_xticklabels([], minor=1)
     ax.set_ylabel('reviews / authors')
     ax.set_title('Reviews / authors per month')
     plt.gca().set_ylim(bottom=0)
-    ax.get_figure().savefig(os.path.join(output, 'reviews_by_authors.png'), bbox_inches='tight')
+    fig.savefig(os.path.join(output, 'reviews_by_authors.png'), bbox_inches='tight')
     plt.close()
 
     # PRs by author
     pr_author_df = prs_by_author.fillna(0.)[:-1]
-    ax = pr_author_df.plot(colormap='tab10', linewidth=2)
+    fig, ax = plt.subplots(figsize=(7, 4))
+    pr_author_df[pr_author_df.columns[::-1]].plot.bar(colormap='tab10', linewidth=2, ax=ax, stacked=True)
+    ax.set_xticklabels(xticklabels, rotation=0)
     ax.set_ylabel('no. merged pull requests')
     ax.set_title('No. merged pull requests by author per month')
     plt.gca().set_ylim(bottom=1)
-    lgd = ax.legend(loc=9, bbox_to_anchor=(1.3, 1.0))
-    ax.get_figure().savefig(os.path.join(output, 'prs_by_author.png'),
-                            additional_artists=[lgd], bbox_inches="tight")
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles[::-1], labels[::-1], loc='upper left')
+    fig.savefig(os.path.join(output, 'prs_by_author.png'), bbox_inches="tight")
     plt.close()
 
     # reviews by reviewer
     reviews_df = reviews_by_person.fillna(0.)[:-1]
-    ax = reviews_df.plot(colormap='tab10', linewidth=2)
+    fig, ax = plt.subplots(figsize=(7, 4))
+    reviews_df[reviews_df.columns[::-1]].plot.bar(colormap='tab10', linewidth=2, ax=ax, stacked=True)
+    ax.set_xticklabels(xticklabels, rotation=0)
     ax.set_ylabel('no. reviews')
     ax.set_title('No. reviews by reviewer per month')
     plt.gca().set_ylim(bottom=1)
-    lgd = ax.legend(loc=9, bbox_to_anchor=(1.3, 1.0))
-    ax.get_figure().savefig(os.path.join(output, 'reviews_by_reviewer.png'),
-                            additional_artists=[lgd], bbox_inches="tight")
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles[::-1], labels[::-1], loc='upper left')
+    fig.savefig(os.path.join(output, 'reviews_by_reviewer.png'), bbox_inches="tight")
     plt.close()
